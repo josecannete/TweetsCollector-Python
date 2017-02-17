a4388686fc956acef08e4634fc1e134347a285a3
Rukua95 <tswysen95@gmail.com>
2017-02-17 13:21:07 -0300

    Update search_person.py

--- Server/search_person.py
+++ Server/search_person.py
@@ -18,16 +18,20 @@ def search(busqueda, tiempo):
 
         if tiempo == 0:
             break
-        ultimo = search_by_user(busqueda, ultimo)
+            
         tiempo -= 1
 
         if tiempo >= 0:
             print ("Busquedas restantes "+str(tiempo)+". Pausa")
         else :
             print ("Pausa")
-
-        if(tiempo != 0):
-            time.sleep(sleep_time)
+            
+        for j in range(3):
+            
+            ultimo = search_by_user(busqueda, ultimo)
+            
+            if(not (tiempo == 0 and j == 2) ):
+                time.sleep(sleep_time)
 
     print ("Fin de busquedas")
     pid = os.getpid()
